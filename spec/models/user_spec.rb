8475f11c583371eb10446fb7e3bd6522bffe13b3
Jason Dorn <apollo701@gmail.com>
2016-03-19 14:46:12 -0700

    validates email presence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   it "fails if emails is blank" do
     user = User.new(email: "   ")
 
     expect(user.valid?).to be_falsey
   end
+
+  it "fails if email is < 8 characters" do
+    user = User.new(email: "@me.com")
+
+    expect(user.valid?).to be_falsey
+  end
 end
+
+@me.com
