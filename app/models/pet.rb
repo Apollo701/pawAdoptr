febaef11209044053d062240961ce735f1f64c4b
Jason Dorn <apollo701@gmail.com>
2016-03-20 23:23:21 -0700

    Add more pet tests iteration 2

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,5 +3,5 @@ class Pet < ActiveRecord::Base
   enum animal: [:dog, :cat]
   validates :name, presence: true, format: { with: VALID_NAME_REGEX }
   validates :animal, inclusion: { in: animals.keys }
-  validates :bio, presence: true, length: { minimum: 155 }
+  validates :bio, presence: true, length: { minimum: 150 }
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,55 +1,72 @@
 require 'rails_helper'
 
 describe Pet, type: :model do
+  before :each do
+    @pet = Pet.new(animal: :dog,
+                   name: "John",
+                   bio: "Lorem ipsum dolor sit amet, consectetur 
+                   adipiscing elit. Sed scelerisque arcu quis fermentum 
+                   tristique. Sed rutrum turpis eget varius volutpat. 
+                   Nullam".squish)
+  end
+
   describe "is a specific type of animal" do
-    it "is a dog" do
-      dog = Pet.new(animal: :dog)
-      
-      expect(dog.animal).to eq("dog")
+    it "is valid as a dog" do
+      @pet.animal = :dog
+
+      expect(@pet.animal).to eq("dog")
     end
 
-    it "is a cat" do
-      cat = Pet.new(animal: :cat)
+    it "is valid as a cat" do
+      @pet.animal = :cat
 
-      expect(cat.animal).to eq("cat")
+      expect(@pet.animal).to eq("cat")
     end
 
-    it "is invalid with name and no animal type" do
-      pet = Pet.new(name: "John")
+    it "is invalid with no animal type" do
+      @pet.animal = nil
 
-      expect(pet).to be_invalid
+      expect(@pet).to be_invalid
     end
   end
 
-  it "is invalid with animal type and no name" do
-    pet = Pet.new(animal: :dog)
+  it "is invalid with no name" do
+    @pet.name = " " * 6
 
-    expect(pet).to be_invalid
-  end
-
-  it "is valid with valid name and animal type" do
-    pet = Pet.new(animal: :cat, name: "John doe")
-
-    expect(pet).to be_valid
+    expect(@pet).to be_invalid
   end
 
   it "is invalid with numerical name" do
-    pet = Pet.new(name: 1245)
+    @pet.name = 1245
 
-    expect(pet).to be_invalid
+    expect(@pet).to be_invalid
   end
 
   it "is invalid with symbols in name" do
-    pet = Pet.new(animal: :dog, name: "John Do$")
+    @pet.name = "John$"
 
-    expect(pet).to be_invalid
+    expect(@pet).to be_invalid
+  end
+
+  it "is invalid with a space in the name" do
+    @pet.name = "John Doe"
+
+    expect(@pet).to be_invalid
   end
 
   it "is invalid with no bio" do
-    pet = Pet.new(bio: "")
+    @pet.bio = ""
 
-    expect(pet.bio).not_to be_empty
+    expect(@pet).to be_invalid
   end
+
+  it "is invalid with bio < 150 characters" do
+    @pet.bio = "Lorem ipsum dolor sit amet, consectetur 
+                   adipiscing elit. Sed scelerisque arcu quis fermentum 
+                   tristique. Sed rutrum turpis eget varius volutpat.".squish
+    expect(@pet).to be_invalid                   
+  end
+
   it "has a breed"
   it "is male or female"
   it "has a valid age"
