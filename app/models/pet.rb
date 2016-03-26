3d3a9061e1cd2feb08eb7ccac0feb25926925d87
Jason Dorn <apollo701@gmail.com>
2016-03-25 23:20:14 -0700

    Finish Pet tests

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,33 +4,13 @@ class Pet < ActiveRecord::Base
   enum sex: [:male, :female]
   enum size: [:small, :medium, :large]
   enum fixed: [:neutered, :spayed]
-  validates :sex, presence: true, inclusion:    { in: sexes.keys }
-  validates :size, presence: true, inclusion:   { in: sizes.keys }
+  validates :sex, presence: true, inclusion:     { in: sexes.keys }
+  validates :size, presence: true, inclusion:    { in: sizes.keys }
   validates :species, presence: true, inclusion: { in: species.keys }
+  validates :fixed, presence: true, inclusion:  { in: fixeds.keys }
   validates :breed, presence: true, format:     { with: VALID_NAME_REGEX }
   validates :name, presence: true, format:      { with: VALID_NAME_REGEX }
   validates :bio, presence: true, length:       { minimum: 150, maximum: 500 }
-  # validates :age, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 30 }
-  # validates :weight, presence: true, numericality: { only_integer: true, greater_than: 3, less_than: 130 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 193 }
+  validates :weight, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 130 }
 end
-
-# require 'spec_helper'
-
-# describe User do
-
-#   before do
-#     @user = User.new(name: "Example User", email: "user@example.com")
-#   end
-
-#   subject { @user }
-
-#   it { should respond_to(:name) }
-#   it { should respond_to(:email) }
-
-#   it { should be_valid }
-
-#   describe "when name is not present" do
-#     before { @user.name = " " }
-#     it { should_not be_valid }
-#   end
-# end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -32,125 +32,211 @@ describe Pet do
 
   subject { @pet } 
 
-  it "belongs to an invalid species" do
-    expect{ @pet.species = :lizard }.to raise_error(ArgumentError)
+    it "belongs to an invalid species" do
+      expect{ @pet.species = :lizard }.to raise_error(ArgumentError)
+    end
+
+    context "has nil species" do
+      before { @pet.species = nil }
+      it { should_not be_valid }
+    end
+
+    context "has no species" do
+      before { @pet.species = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "has no name" do
+      before { @pet.name = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "has nil name" do
+      before { @pet.species = nil }
+
+      it { should_not be_valid }
+    end
+
+    context "has numerical name" do
+      before { @pet.name = 12345 }
+
+      it { should_not be_valid }
+    end
+
+    context "has symbols name" do
+      before { @pet.name = "$&^" }
+
+      it { should_not be_valid }
+    end
+
+    context "has no sex" do
+      before { @pet.sex = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "has nil sex" do
+      before { @pet.sex = nil }
+
+      it { should_not be_valid }
+    end
+
+    it "has invalid sex" do
+      expect { @pet.sex = :giraffe }.to raise_error(ArgumentError)
+    end
+
+    context "has no size" do
+      before { @pet.size = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "has nil size" do
+      before { @pet.size = nil }
+
+      it { should_not be_valid }
+    end
+
+    it "has invalid size" do
+      expect { @pet.size = :extraLarge }.to raise_error(ArgumentError)
+    end
+
+    context "has no breed" do
+      before { @pet.breed = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "has nil breed" do
+      before { @pet.breed = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "has numerical breed" do
+      before { @pet.breed = 12345 }
+
+      it { should_not be_valid }
+    end
+
+    context "has symbols breed" do
+      before { @pet.breed = "&^%" }
+
+      it { should_not be_valid }
+    end
+
+    context "has no bio" do
+      before { @pet.bio = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "has nil bio" do
+      before { @pet.bio = nil }
+
+      it { should_not be_valid }
+    end
+
+    context "has bio < 150 characters" do
+      before { @pet.bio = "a" * 149 }
+
+      it { should_not be_valid }
+    end
+
+    context "has bio > 500 characters" do
+      before { @pet.bio = "a" * 501 }
+
+      it { should_not be_valid }
+    end
+
+    context "it has a negative age" do
+      before { @pet.age = -1 }
+
+      it { should_not be_valid }
+    end
+
+    context "it is 193 months old" do
+      before { @pet.age = 193 }
+
+      it { should_not be_valid }
+    end
+
+    context "it has a string age" do
+      before { @pet.age = "age" }
+
+      it { should_not be_valid }
+    end
+
+    context "it has no age" do
+      before { @pet.age = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "it has nil age" do
+      before { @pet.age = nil }
+
+      it { should_not be_valid }
+    end
+
+    context "it has decimal age" do
+      before { @pet.age = 1.2 }
+
+      it { should_not be_valid }
+    end
+
+    context "it has no weight" do
+      before { @pet.weight = " " }
+
+      it { should_not be_valid }
+    end
+
+    context "it has nil weight" do
+      before { @pet.weight = nil }
+
+      it { should_not be_valid }
+    end
+
+    context "it has string weight" do
+      before { @pet.weight = "weight" }
+
+      it { should_not be_valid }
+    end
+
+    context "it has negative weight" do
+      before { @pet.weight = -5 }
+
+      it { should_not be_valid }
+    end
+
+    context "it has decimal weight" do
+      before { @pet.weight = 1.2 }
+
+      it { should_not be_valid }
+    end
+
+    context "it has invalid weight" do
+      before { @pet.weight = 131 }
+
+      it { should_not be_valid }
+    end
+    
+    context "is not fixed" do
+      before { @pet.fixed = "" }
+
+      it { should_not be_valid }
+    end
+
+    context "is nil fixed" do
+      before { @pet.fixed = nil }
+
+      it { should_not be_valid }
+    end
+
+    it "is invalid fixed" do
+      expect { @pet.fixed = :nope }.to raise_error(ArgumentError)
+    end
   end
-
-  context "has nil species" do
-    before { @pet.species = nil }
-    it { should_not be_valid }
-  end
-
-  context "has no species" do
-    before { @pet.species = "" }
-
-    it { should_not be_valid }
-  end
-
-  context "has no name" do
-    before { @pet.name = "" }
-
-    it { should_not be_valid }
-  end
-
-  context "has nil name" do
-    before { @pet.species = nil }
-
-    it { should_not be_valid }
-  end
-
-  context "has numerical name" do
-    before { @pet.name = 12345 }
-
-    it { should_not be_valid }
-  end
-
-  context "has symbols name" do
-    before { @pet.name = "$&^" }
-
-    it { should_not be_valid }
-  end
-
-  context "has no sex" do
-    before { @pet.sex = "" }
-
-    it { should_not be_valid }
-  end
-
-  context "has nil sex" do
-    before { @pet.sex = nil }
-
-    it { should_not be_valid }
-  end
-
-  it "has invalid sex" do
-    expect { @pet.sex = :giraffe }.to raise_error(ArgumentError)
-  end
-
-  context "has no size" do
-    before { @pet.size = "" }
-
-    it { should_not be_valid }
-  end
-
-  context "has nil size" do
-    before { @pet.size = nil }
-
-    it { should_not be_valid }
-  end
-
-  it "has invalid size" do
-    expect { @pet.size = :extraLarge }.to raise_error(ArgumentError)
-  end
-
-  context "has no breed" do
-    before { @pet.breed = "" }
-
-    it { should_not be_valid }
-  end
-
-  context "has nil breed" do
-    before { @pet.breed = "" }
-
-    it { should_not be_valid }
-  end
-
-  context "has numerical breed" do
-    before { @pet.breed = 12345 }
-
-    it { should_not be_valid }
-  end
-
-  context "has symbols breed" do
-    before { @pet.breed = "&^%" }
-
-    it { should_not be_valid }
-  end
-
-  context "has no bio" do
-    before { @pet.bio = "" }
-
-    it { should_not be_valid }
-  end
-
-  context "has nil bio" do
-    before { @pet.bio = nil }
-
-    it { should_not be_valid }
-  end
-
-  context "has bio < 150 characters" do
-    before { @pet.bio = "a" * 149 }
-
-    it { should_not be_valid }
-  end
-
-  context "has bio > 500 characters" do
-    before { @pet.bio = "a" * 501 }
-
-    it { should_not be_valid }
-  end
-  
-  it "is neutered or spayed"
-end
 end
